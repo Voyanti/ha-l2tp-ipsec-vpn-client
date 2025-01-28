9b3f3ad829c727d98408093073fad4d7a831e8eb
Heinrich de Lange <heinrich.delange@gmail.com>
2025-01-28 20:52:22 +0200

    NA

--- l2tp_ipsec_vpn_client/run.sh
+++ l2tp_ipsec_vpn_client/run.sh
@@ -46,32 +46,4 @@ echo "=== /etc/ppp/chap-secrets ==="
 cat /etc/ppp/chap-secrets
 echo
 
-echo "Stopping any existing StrongSwan and xl2tpd processes..."
-pkill -f charon
-pkill -f xl2tpd
-
-sleep 2
-
-echo "Starting StrongSwan VPN connection..."
-ipsec start
-
-sleep 5  # Allow time for StrongSwan to initialize
-
-echo "Bringing up the IPsec tunnel..."
-ipsec up L2TP-IPsec
-
-sleep 3  # Allow time for negotiation
-
-echo "Starting xl2tpd..."
-mkdir -p /var/run/xl2tpd
-xl2tpd -D &
-
-sleep 7  # Give xl2tpd some time to start
-
-echo "Sending connection command to xl2tpd..."
-echo "c L2TP-IPsec" > /var/run/xl2tpd/l2tp-control
-
-echo "VPN connection started."
-
-# Keep the container running
-exec tail -f /dev/null
+exec /usr/bin/supervisord -c /supervisord.conf
