f4423720a7a7d6ff7dcac46566f1971e9ae4c9fe
Heinrich de Lange <heinrich.delange@gmail.com>
2025-01-28 20:54:49 +0200

    NA

--- l2tp_ipsec_vpn_client/run.sh
+++ l2tp_ipsec_vpn_client/run.sh
@@ -46,4 +46,20 @@ echo "=== /etc/ppp/chap-secrets ==="
 cat /etc/ppp/chap-secrets
 echo
 
-exec /usr/bin/supervisord -c /supervisord.conf
+# startup ipsec tunnel
+ipsec initnss
+sleep 1
+ipsec pluto --stderrlog --config /etc/ipsec.conf
+sleep 5
+#ipsec setup start
+#sleep 1
+#ipsec auto --add L2TP-PSK
+#sleep 1
+ipsec auto --up L2TP-PSK
+sleep 3
+ipsec --status
+sleep 3
+
+# startup xl2tpd ppp daemon then send it a connect command
+(sleep 7 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
+exec /usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D
